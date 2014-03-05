919bb977f53160f99c45d57a6692c7b947ba6ac2
dailawa <dailawadi@walmartlabs.com>
2014-03-05 15:59:43 -0800

    Initial grosla app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grosla::Application.config.secret_key_base = 'a60ec238793c7608c35a4f47cfd6294a756bd7ad8a389e4e12552fade09359fbf52413544414a392ae521eabf3cb48a685101959ba1fa2f86b7af986dfaaefc2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Grosla::Application.config.session_store :cookie_store, key: '_grosla_session'
